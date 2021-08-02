b27fae22df1024ce632baedf957dd467fc8afc05
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2021-08-01 22:01:18 -0700

    Section 5 Finished
    added message & loader components and actions for details

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -12,7 +12,7 @@ router.get(
 	asyncHandler(async (req, res) => {
 		const products = await Product.find({});
 		// res.status(401);
-		// throw new Error("Not Authorized");
+		// throw new Error("Not Authorized"); // for testing
 		res.json(products);
 	})
 );
